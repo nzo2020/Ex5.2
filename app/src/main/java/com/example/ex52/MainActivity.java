fe8865f15af9901c74c4f1b532e7fe3d71376de8
nzo2020 <nz2020@bs.amalnet.k12.il>
2024-01-02 00:35:20 +0200

    Ex5.2

--- app/src/main/java/com/example/ex52/MainActivity.java
+++ app/src/main/java/com/example/ex52/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     Button btn;
 
     TextView tView;
-
     int clickNumber=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
